2b79810990738efed3ac9943340c64aa8a69130a
Luba Moura <luba11751@gmail.com>
2026-09-14 15:07:29 -0500

    Add automated testing Docker and CI workflow

--- src/evaluate.py
+++ src/evaluate.py
@@ -3,6 +3,7 @@
 Metrics go to a metrics.json file as well as the console, because
 later an automated workflow will read that file to decide whether
 the model is good enough to deploy.
+Just Check in 
 """
 
 import argparse
